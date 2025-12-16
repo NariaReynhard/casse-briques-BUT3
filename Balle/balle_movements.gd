8a131e382ccd0566e78d190644cc28d1fc07455e
NariaReynhard <nariareynhard@gmail.com>
2025-12-16 11:21:30 +0100

    ajout de murs

--- Balle/balle_movements.gd
+++ Balle/balle_movements.gd
@@ -1,14 +1,22 @@
 extends Node
 
-# Called when the node enters the scene tree for the first time.
+@export var vitesse_cible := 200.0  # pixels/s
+
+var balle : RigidBody2D
+
 func _ready() -> void:
-	var balle := get_parent() as RigidBody2D
+	balle = get_parent() as RigidBody2D
 	balle.gravity_scale = 0.0
 	balle.linear_damp = 0.0
-	balle.lock_rotation=true
-	balle.apply_impulse(Vector2(0,-500))
+	balle.lock_rotation = true
+	
+	# Attendre un frame pour s'assurer que tout est chargé
+	await get_tree().process_frame
+	
+	# Impulsion initiale vers le haut
+	balle.apply_impulse(Vector2(0, -1) * vitesse_cible)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _physics_process(_delta: float) -> void:
+	if balle and balle.linear_velocity.length() != 0:
+		# Conserver la vitesse constante à chaque frame
+		balle.linear_velocity = balle.linear_velocity.normalized() * vitesse_cible
